6d2090da9c366392b0649a97a96dd0ae15b024bb
vungjoe <vungjoe@gmail.com>
2018-10-16 00:00:22 +0800

    这个脚本范了2个错误，一个是图片地址不是网址（刚好网址也是.jpg结尾），二是wb错写成了w。

---
+++ 005-ScrapingPics.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+import requests
+import os
+
+url = 'https://www.nationalgeographic.com/content/dam/photography/rights-exempt/photos-of-the-week/181012-potw/01-potw-181012.adapt.885.1.jpg'
+root = 'D://nationalgeographic//'
+path = root + url.split('/')[-1]
+
+try:
+    if not os.path.exists(root):
+        os.mkdir(root)
+    if not os.path.exists(path):
+        r = requests.get(url)
+        with open(path,'wb') as f:
+            f.write(r.content)
+            f.close()
+        print('文件保存成功')
+    else:
+        print('文件已存在')
+except:
+    print('爬取失败')
